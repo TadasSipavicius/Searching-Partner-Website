f8f8b474bb42395def3c432f551cfd3a56ef4165
TadasSipavicius <tasipas@gmail.com>
2021-06-20 17:31:21 +0300

    Final color for now

--- src/theme.tsx
+++ src/theme.tsx
@@ -3,7 +3,7 @@ import { createMuiTheme } from "@material-ui/core/styles";
 const theme = createMuiTheme({
     palette: {
         primary: {
-            main: "#057010",
+            main: "#23932e",
         },
     }
 })
