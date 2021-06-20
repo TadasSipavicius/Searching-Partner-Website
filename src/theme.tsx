95ec9d8d52e81198683f632bf7169cfb55b743a3
TadasSipavicius <tasipas@gmail.com>
2021-06-20 17:21:14 +0300

    Testing colors again

--- src/theme.tsx
+++ src/theme.tsx
@@ -3,7 +3,7 @@ import { createMuiTheme } from "@material-ui/core/styles";
 const theme = createMuiTheme({
     palette: {
         primary: {
-            main: "#23932e",
+            main: "#057010",
         },
     }
 })
