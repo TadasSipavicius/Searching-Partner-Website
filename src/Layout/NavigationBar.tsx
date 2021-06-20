2b10582b0b9072870dd51784b5bbf300829c5b87
TadasSipavicius <tasipas@gmail.com>
2021-06-20 19:23:06 +0300

    Added NavBar links

--- src/Layout/NavigationBar.tsx
+++ src/Layout/NavigationBar.tsx
@@ -1,15 +1,23 @@
-import { AppBar } from '@material-ui/core';
+import { AppBar, Button, Grid, Link } from '@material-ui/core';
 import { Toolbar } from '@material-ui/core';
 import { Typography } from '@material-ui/core';
 import { createStyles, makeStyles } from '@material-ui/core';
 import { Theme } from '@material-ui/core/styles';
+
+import { navigation, NavigationType } from '../Navigation';
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         appBar: {
             backgroundColor: "#ECECEC",
+            justifyContent: "space-between",
+        },
+        toolbar: {
+            width: '80%',
+            margin: 'auto',
         },
         logoTitle: {
             display: "flex",
@@ -28,6 +36,13 @@ const useStyles = makeStyles((theme: Theme) =>
             backgroundColor: "black",
             borderRadius: 10,
         },
+        navlinks: {
+            textTransform: 'uppercase',
+            fontWeight: 'bold',
+            margin: theme.spacing(1,2),
+            color: "black",
+            fontFamily: "Quintessential, cursive",
+        }
 
 }));
 export default function NavigationBar(){
@@ -35,12 +50,22 @@ export default function NavigationBar(){
     const classes = useStyles();
     return(
         <AppBar className={classes.appBar} elevation={4}>
-            <Toolbar>
+            <Toolbar className={classes.toolbar}>
                 <Typography className={classes.logoTitle}>
                     Partner 
                     <Typography className={classes.lineBetween} />
                     Finderis
                 </Typography>
+                <Grid item xs />
+                <Typography style={{ float: "right" }}>
+                    {navigation.map((item: NavigationType) => (
+                        <Link component={NavLink} to={item.to} className={classes.navlinks}>
+                            {item.title}
+                        </Link>
+                    ))}
+                </Typography>
+                <Button>Login</Button>
+                <Button>Register</Button>
             </Toolbar>
         </AppBar>
     )
