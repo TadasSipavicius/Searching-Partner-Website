62714d2c73b805f664af4d5868cecef38eaf3fc6
TadasSipavicius <tasipas@gmail.com>
2021-07-27 21:32:55 +0300

    Updated loginrequirement page

--- Client/src/Pages/LoginRequirement.tsx
+++ Client/src/Pages/LoginRequirement.tsx
@@ -2,15 +2,19 @@ import { Typography } from '@material-ui/core';
 import React from 'react';
 
 import PageContainer from '../Components/PageContainer';
-
+import AuthLoginButton from '../Components/Auth0/AuthLoginButton';
 
 export default function LoginRequirements(){
 
     return(
         <PageContainer>
             <Typography>
-                U must log in
+                If you want to create a post, then you must Log in 
             </Typography>
+            <Typography>
+                Click Button Below
+            </Typography>
+            <AuthLoginButton name="Login"/>
         </PageContainer>
     )
 }
