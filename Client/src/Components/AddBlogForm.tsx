8d279eb6a6ef38705d448e8717827070c5870f61
TadasSipavicius <tasipas@gmail.com>
2021-12-06 00:20:13 +0200

    Added CreateBlog component into Blog page

---
+++ Client/src/Components/AddBlogForm.tsx
@@ -0,0 +1,12 @@
+import { Container, Typography } from '@material-ui/core';
+import React from 'react';
+
+export default function AddBlogForm(){
+    return(
+        <Container>
+            <Typography>
+                AddBlog
+            </Typography>
+        </Container>
+    )
+}
--- Client/src/Pages/Blog.tsx
+++ Client/src/Pages/Blog.tsx
@@ -1,15 +1,31 @@
-import { Typography } from '@material-ui/core';
+import { Button, Typography } from '@material-ui/core';
 import React from 'react';
 
 import PageContainer from '../Components/PageContainer';
 import ContactForm from '../Components/ContactForm';
+import { useAuth0 } from '@auth0/auth0-react';
+import { useHistory } from 'react-router';
 export default function Blog() {
 
+    const { user } = useAuth0();
+    const adminID = process.env.REACT_APP_ADMINISTR_ID;
+    const history = useHistory();
+    
+    const handleAddBlog = () =>{
+        history.push('./createblog')
+    }
     return(
         <PageContainer>
             <Typography>
                 BLOG
             </Typography>
+
+            {user?.sub === adminID  ?
+            <Button onClick={handleAddBlog}>
+                Add Blog
+            </Button> : 
+            null}
+
             <ContactForm />
         </PageContainer>
 
