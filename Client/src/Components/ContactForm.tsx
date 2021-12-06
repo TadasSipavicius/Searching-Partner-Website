7723f8321d594d6da20d47acaed75e8b9cb81757
TadasSipavicius <tasipas@gmail.com>
2021-12-06 02:58:35 +0200

    Added CurrentYear in ContactForm

--- Client/src/Components/ContactForm.tsx
+++ Client/src/Components/ContactForm.tsx
@@ -1,22 +1,30 @@
-import { Container, createStyles, makeStyles, Theme, Typography } from '@material-ui/core';
+import { Container, createStyles, Divider, makeStyles, Theme, Typography } from '@material-ui/core';
 import React from 'react';
 
 const useStyles = makeStyles( (theme: Theme) => 
     createStyles({
         contactForm: {
             position: "absolute",
-            bottom: 0
+            bottom: 0,
+            textAlign: "center"
         }
     }))
-    
+
+const getCurrentYear = () =>{
+    return new Date().getFullYear();
+}
 export default function ContactForm(){
 
     const classes = useStyles();
 
     return(
         <Container className={classes.contactForm}>
+            <Divider />
             <Typography>
                 ContactForm
+            </Typography>
+            <Typography>
+                © PartnerFinderis {getCurrentYear()}
             </Typography>
         </Container>
 
