7cf42edb15eeb2ca1217ad78e3f0b3916a954c33
TadasSipavicius <tasipas@gmail.com>
2021-12-07 14:28:25 +0200

    Added websites cards prototypes

---
+++ Client/src/Components/Cards/BlogCard.tsx
@@ -0,0 +1,10 @@
+import { Container } from '@material-ui/core';
+import React from 'react';
+
+export default function BlogCard(){
+    return(
+        <Container>
+            Blog Card
+        </Container>
+    )
+}
---
+++ Client/src/Components/Cards/FindPlayerCard.tsx
@@ -0,0 +1,10 @@
+import { Container } from '@material-ui/core';
+import React from 'react';
+
+export default function FindPlayerCard(){
+    return(
+        <Container>
+            FindPlayerCard
+        </Container>
+    )
+}
